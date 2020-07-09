crashdev226/serveless

commit b08c863ee85178529aecddf2a178ba3454cca511

refactor: Replace `_.has` with better counterparts (#7915)

lib/plugins/aws/package/compile/events/cognitoUserPool/index.js
@@ -350,7 +350,7 @@ class AwsCompileCognitoUserPoolEvents {
       const userPoolLogicalId = this.provider.naming.getCognitoUserPoolLogicalId(poolName);
 
       // If overrides exist in `Resources`, merge them in
-      if (_.has(this.serverless.service.resources, userPoolLogicalId)) {
+      if (this.serverless.service.resources[userPoolLogicalId]) {
         const customUserPool = this.serverless.service.resources[userPoolLogicalId];
         const generatedUserPool = this.generateTemplateForPool(
           poolName,

lib/plugins/aws/deployFunction/index.js
@@ -225,7 +225,7 @@ class AwsDeployFunction {
 
     // check if an artifact is used in function package level
     const functionObject = this.serverless.service.getFunction(this.options.function);
-    if (_.has(functionObject, ['package', 'artifact'])) {
+    if (_.get(functionObject, 'package.artifact')) {
       artifactFilePath = functionObject.package.artifact;
     }
 

lib/plugins/aws/package/compile/events/schedule/index.js
@@ -69,7 +69,7 @@ class AwsCompileScheduledEvents {
               }
 
               if (Input && typeof Input === 'object') {
-                if (_.has(Input, 'body') && typeof Input.body === 'string') {
+                if (typeof Input.body === 'string') {
                   try {
                     Input.body = JSON.parse(Input.body);
                   } catch (error) {

lib/plugins/aws/package/compile/events/sns/index.js
@@ -29,13 +29,13 @@ class AwsCompileSNSEvents {
       return false;
     }
     if (
-      _.has(variable, 'Fn::ImportValue') &&
-      (_.has(variable, 'Fn::ImportValue.Fn::GetAtt') || _.has(variable, 'Fn::ImportValue.Ref'))
+      variable['Fn::ImportValue'] &&
+      (variable['Fn::ImportValue']['Fn::GetAtt'] || variable['Fn::ImportValue'].Ref)
     ) {
       return false;
     }
     const intrinsicFunctions = ['Fn::ImportValue', 'Ref', 'Fn::GetAtt', 'Fn::Sub', 'Fn::Join'];
-    return intrinsicFunctions.some(func => _.has(variable, func));
+    return intrinsicFunctions.some(func => variable[func]);
   }
 
   compileSNSEvents() {

lib/plugins/aws/package/compile/events/sqs/index.js
@@ -45,9 +45,9 @@ class AwsCompileSQSEvents {
                 if (
                   Object.keys(event.sqs.arn).length !== 1 ||
                   !(
-                    _.has(event.sqs.arn, 'Fn::ImportValue') ||
-                    _.has(event.sqs.arn, 'Fn::GetAtt') ||
-                    _.has(event.sqs.arn, 'Fn::Join')
+                    event.sqs.arn['Fn::ImportValue'] ||
+                    event.sqs.arn['Fn::GetAtt'] ||
+                    event.sqs.arn['Fn::Join']
                   )
                 ) {
                   const errorMessage = [

lib/plugins/aws/package/compile/events/stream/index.js
@@ -124,11 +124,11 @@ class AwsCompileStreamEvents {
                 if (
                   Object.keys(event.stream.arn).length !== 1 ||
                   !(
-                    _.has(event.stream.arn, 'Fn::ImportValue') ||
-                    _.has(event.stream.arn, 'Fn::GetAtt') ||
-                    (_.has(event.stream.arn, 'Ref') &&
-                      _.has(this.serverless.service.resources.Parameters, event.stream.arn.Ref)) ||
-                    _.has(event.stream.arn, 'Fn::Join')
+                    event.stream.arn['Fn::ImportValue'] ||
+                    event.stream.arn['Fn::GetAtt'] ||
+                    (event.stream.arn.Ref &&
+                      this.serverless.service.resources.Parameters[event.stream.arn.Ref]) ||
+                    event.stream.arn['Fn::Join']
                   )
                 ) {
                   const errorMessage = [

lib/classes/CLI.js
@@ -255,7 +255,7 @@ functionalities related to given service or current environment.`
 
       // check if a plugin entry is already present in pluginCommands. Use the
       // existing one or create a new plugin entry.
-      if (_.has(pluginCommands, pcmd.pluginName)) {
+      if (pluginCommands[pcmd.pluginName]) {
         pluginCommands[pcmd.pluginName] = pluginCommands[pcmd.pluginName].concat(pcmd);
       } else {
         pluginCommands[pcmd.pluginName] = [pcmd];

lib/classes/PluginManager.js
@@ -261,7 +261,7 @@ class PluginManager {
     _.forEach(pluginInstance.hooks, (hook, event) => {
       let target = event;
       const baseEvent = event.replace(/^(?:after:|before:)/, '');
-      if (_.has(this.deprecatedEvents, baseEvent)) {
+      if (this.deprecatedEvents[baseEvent]) {
         const redirectedEvent = this.deprecatedEvents[baseEvent];
         if (process.env.SLS_DEBUG) {
           this.serverless.cli.log(`WARNING: Plugin ${pluginName} uses deprecated hook ${event},

commit 2557709ce1c49472212e95d14c1e741f28a774b3

fix(AWS Cognito):  FIx properly resource resolution
Regression originally introduced with #7915

lib/plugins/aws/package/compile/events/cognitoUserPool/index.js
@@ -350,7 +350,7 @@ class AwsCompileCognitoUserPoolEvents {
       const userPoolLogicalId = this.provider.naming.getCognitoUserPoolLogicalId(poolName);
 
       // If overrides exist in `Resources`, merge them in
-      if (_.get(this.serverless.service.resources, 'userPoolLogicalId')) {
+      if (_.get(this.serverless.service.resources, userPoolLogicalId)) {
         const customUserPool = this.serverless.service.resources[userPoolLogicalId];
         const generatedUserPool = this.generateTemplateForPool(
           poolName,